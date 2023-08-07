24b118bd333fd1dd3652f199f7b7bbbcb94a4ccc
kraigochieng <kraigochieng@gmail.com>
2023-08-07 12:54:13 +0300

    Removed Optional dependency dou to no usage

--- server/src/main/java/dev/kraigochieng/patient_visit_system/server/repositories/PatientRepository.java
+++ server/src/main/java/dev/kraigochieng/patient_visit_system/server/repositories/PatientRepository.java
@@ -4,7 +4,6 @@ import dev.kraigochieng.patient_visit_system.server.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
