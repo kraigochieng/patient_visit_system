a1c3b27529c7f3173c6958655d73a00486372b37
kraigochieng <kraigochieng@gmail.com>
2023-08-07 08:24:47 +0300

    Created server directory

---
+++ server/src/main/java/dev/kraigochieng/patient_visit_system/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package dev.kraigochieng.patient_visit_system.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/kraigochieng/patient_visit_system/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kraigochieng.patient_visit_system.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
