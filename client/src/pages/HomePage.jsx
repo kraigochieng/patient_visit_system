0dbfe8c8cee274ab6a2cda5f4ab5e849eec88e72
kraigochieng <kraigochieng@gmail.com>
2023-08-08 11:35:50 +0300

    Created home page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+export default function HomePage() {
+  return (
+    <div>
+      <div id="menu">
+        <Link to="/registration"><button>Add Patient</button></Link>
+        <Link to="/patient-search"><button>Record Visit</button></Link>
+        <Link><button>Patient Listing</button></Link>
+      </div>
+    </div>
+    
+  )
+}
