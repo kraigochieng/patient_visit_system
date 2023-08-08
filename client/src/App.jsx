4325cd51d6c370e4a9e8278da093229565d6acb7
kraigochieng <kraigochieng@gmail.com>
2023-08-08 11:37:29 +0300

    Created application routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 import RegistrationForm from './components/forms/RegistrationForm'
-
+import {Link, Routes, Route} from  "react-router-dom"
+import HomePage from './pages/HomePage'
+import VisitForm from './components/forms/VisitForm'
+import PatientSearchForm from './components/forms/PatientSearchForm'
+import HomeNavbar from './components/navbar/HomeNavbar'
 function App() {
-  const [count, setCount] = useState(0)
 
-  
   return (
-    <div>
-      <RegistrationForm />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomeNavbar />}>
+        <Route index element={<HomePage />}/>
+        <Route path="registration" element={<RegistrationForm />} />
+        <Route path="patient-search" element={<PatientSearchForm/> } />
+        <Route path="visit" element={<VisitForm/>} />
+      </Route>
+    </Routes>
   )
 }
 
