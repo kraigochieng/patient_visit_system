c59fc810b9e7efc0116e6364902d8904d3f87533
kraigochieng <kraigochieng@gmail.com>
2023-08-08 15:53:06 +0300

    Added methods

--- server/src/main/java/dev/kraigochieng/patient_visit_system/server/services/PatientService.java
+++ server/src/main/java/dev/kraigochieng/patient_visit_system/server/services/PatientService.java
@@ -3,9 +3,11 @@ package dev.kraigochieng.patient_visit_system.server.services;
 
 import dev.kraigochieng.patient_visit_system.server.models.Patient;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PatientService {
     public List<Patient> getPatients();
     public Patient postPatient(Patient patient);
+    public List<Patient> getPatientsByRegistrationDate(String registrationDateAsString);
 }
