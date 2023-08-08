fdc0e90c3560f9055650f37342468ecc9e51cd44
kraigochieng <kraigochieng@gmail.com>
2023-08-08 11:43:41 +0300

    Fixed the error on the @RequestMapping path parameter

--- server/src/main/java/dev/kraigochieng/patient_visit_system/server/controllers/GenderController.java
+++ server/src/main/java/dev/kraigochieng/patient_visit_system/server/controllers/GenderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = " api/v1/genders")
+@RequestMapping(path = "api/v1/genders")
 @CrossOrigin
 public class GenderController {
     @Autowired
