24264bc646b74361ea557301cead4502abf0056b
kraigochieng <kraigochieng@gmail.com>
2023-08-08 15:43:17 +0300

    Created Patient Listing Report

---
+++ client/src/components/reports/PatientListing.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import { server } from '../../axiosInstances'
+
+export default function PatientListing() {
+
+   const [patients, setPatients] = useState([])
+
+    const [patientListingForm, setPatientListingForm] = useState({
+        date: new Date().toISOString().split("T")[0]
+    })
+
+
+    // Get the patients by the regsitration date
+    useEffect(() => {
+        server.get(`patients/get/registrationDate/${patientListingForm.date}`)
+        .then(response => setPatients(response.data))
+        .then(setPatients(prevPatients => (
+            prevPatients.sort((a,b) => new Date(a.dateOfVisit) - new Date(b.dateOfVisit))
+        )))
+    }, [patientListingForm.date])
+
+    function PatientListingRow(props) {
+        const {firstName, lastName, visits, dateOfBirth} = props.patient
+        return (
+            <tr>
+                <td>{firstName} {lastName}</td>
+                <td>{calculateAge(dateOfBirth)}</td>
+                <td>{visits[0].bmiStatus}</td>
+            </tr>
+        )
+    }
+
+
+    function calculateAge(birthDateString) {
+        const today = new Date();
+        const birthDate = new Date(birthDateString)
+      
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+      
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+      
+        return age;
+      }
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setPatientListingForm(prevPatientListingForm => ({
+            ...prevPatientListingForm,
+            [name]: value
+        }))
+    }
+    return (
+        <div>
+            <form>
+                <input type="date" name="date" value={patientListingForm.date} onChange={handleChange} max={new Date().toISOString().split("T")[0]} />
+            </form>
+            {
+                patients.length === 0 ?
+                <p>No patients have been registered on {patientListingForm.date}</p>:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Full Names</th>
+                            <th>Age</th>
+                            <th>BMI Status</th>
+                        </tr>
+                    </thead>
+                        
+                    <tbody>
+                        {patients.map(patient => <PatientListingRow key={patient.id} patient={patient} />)}
+                    </tbody>
+                </table>
+            }
+        </div>
+
+    )
+}
