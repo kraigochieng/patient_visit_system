f89c4c52b3ada11c11dd18db221c1197763445d0
kraigochieng <kraigochieng@gmail.com>
2023-08-08 15:50:38 +0300

    Added getPatientByRegistrationDate method

--- server/src/main/java/dev/kraigochieng/patient_visit_system/server/repositories/PatientRepository.java
+++ server/src/main/java/dev/kraigochieng/patient_visit_system/server/repositories/PatientRepository.java
@@ -2,11 +2,16 @@ package dev.kraigochieng.patient_visit_system.server.repositories;
 
 import dev.kraigochieng.patient_visit_system.server.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
-
+    @Query("SELECT p FROM Patient p WHERE p.registeredAt = :registrationDate")
+    List<Patient> getPatientsByRegistrationDate(LocalDate registrationDate);
 }
